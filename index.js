abf448ecafc772f5d8d95425f1b8fe24314fdcf1
rahmam <mohammed.rahman@cdk.com>
2017-09-20 16:41:31 +0530

    more changes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT | 5000;
-app.listen(port, () => console.log("running"));
+app.listen(port, function () {
+    console.log("working");
+});
 
-app.get('/', (req, res) => res.send("WOkring"));
+app.get('/', function (req, res) {
+    res.send("WOkring");
+});
